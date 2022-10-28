38e5bcefa673be1ae13a298a6e9274303894c1a5
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-10-28 16:25:37 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,24 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
-}
+# install and set up nginx
 
-file_line { 'Redirect':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+package { 'nginx':
+  ensure => 'installed',
 }
 
 file { '/var/www/html/index.html':
+  require => Package['nginx'],
   content => 'Hello World!',
+}
+
+file_line { 'add-rewrite':
+  ensure  => 'present',
+  require => Package['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'root /var/www/html;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+  notify  => Service['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx'],
+  require => File_line['add-rewrite'],
 }
